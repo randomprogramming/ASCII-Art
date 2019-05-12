acd4ed9c87480c2021c1089a1b868a84ede0a259
randomprogramming <isnipedodac@gmail.com>
2019-05-12 11:41:35 +0200

    First working version

--- Density.java
+++ Density.java
@@ -1,11 +1,20 @@
 enum Density {
-    LOW("."),
-    MID("/"),
-    HIGH("&");
+    DENSITY("\"`^\\\",:;Il!i~+_-?][}{1)(|\\\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$\"");
 
     private final String density;
 
     Density(String density){
         this.density = density;
+    }
+    char getDensityFor(int value){
+        double percent = this.density.length() / 255.0;
+        int charValue = (int) (Math.round(percent * value));
+        if(charValue<0){
+            charValue = 0;
+        }
+        else if(charValue >= this.density.length()){
+            charValue = this.density.length() - 1;
+        }
+        return this.density.charAt(charValue);
     }
 }
--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args){
-        ImageReading test = new ImageReading("D:\\pic\\test.png");
+        ImageReading test = new ImageReading("D:\\pic\\download.png");
         int[][] colors= test.getImageBrightness();
 
         for (var i = 0; i < test.getImageWidth(); i++) {
             for (var j = 0; j < test.getImageHeight(); j++) {
-                System.out.print(colors[i][j]+", ");
+                System.out.print(Density.DENSITY.getDensityFor(colors[i][j]));
             }
             System.out.println();
         }
